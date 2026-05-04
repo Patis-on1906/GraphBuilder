bd3fee034d02633d5a44d522587af1b54f7d7e80
Vlad Leontev <vleontev979@gmail.com>
2026-05-04 18:18:53 +0700

    final project skeleton

--- GraphBuilder/Models/GraphEdge.cs
+++ GraphBuilder/Models/GraphEdge.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -7,23 +8,69 @@ public class GraphEdge : INotifyPropertyChanged
 {
     public int AbsoluteId { get; init; }
     public int LocalId { get; init; }
-    public int TargetNodeId { get; set; }
 
-    // Координаты начальной и конечной точек
-    public double X1 { get; set; }
-    public double Y1 { get; set; }
-    public double X2 { get; set; }
-    public double Y2 { get; set; }
+    private int _targetNodeId;
+    public int TargetNodeId
+    {
+        get => _targetNodeId;
+        set
+        {
+            if (_targetNodeId != value)
+            {
+                _targetNodeId = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
-    public int Predicate { get; set; }
-    public double DelaySeconds { get; set; } = 2.0;
+    private double _x1;
+    public double X1
+    {
+        get => _x1;
+        set { if (Math.Abs(_x1 - value) > 0.001) { _x1 = value; OnPropertyChanged(); } }
+    }
+
+    private double _y1;
+    public double Y1
+    {
+        get => _y1;
+        set { if (Math.Abs(_y1 - value) > 0.001) { _y1 = value; OnPropertyChanged(); } }
+    }
+
+    private double _x2;
+    public double X2
+    {
+        get => _x2;
+        set { if (Math.Abs(_x2 - value) > 0.001) { _x2 = value; OnPropertyChanged(); } }
+    }
+
+    private double _y2;
+    public double Y2
+    {
+        get => _y2;
+        set { if (Math.Abs(_y2 - value) > 0.001) { _y2 = value; OnPropertyChanged(); } }
+    }
+
+    private int _predicate;
+    public int Predicate
+    {
+        get => _predicate;
+        set { if (_predicate != value) { _predicate = value; OnPropertyChanged(); } }
+    }
+
+    private double _delaySeconds = 2.0;
+    public double DelaySeconds
+    {
+        get => _delaySeconds;
+        set { if (Math.Abs(_delaySeconds - value) > 0.001) { _delaySeconds = value; OnPropertyChanged(); } }
+    }
 
     public GraphEdge(int absoluteId, int localId, int targetId, double x1, double y1, double x2, double y2)
     {
         AbsoluteId = absoluteId;
         LocalId = localId;
-        TargetNodeId = targetId;
-        X1 = x1; Y1 = y1; X2 = x2; Y2 = y2;
+        _targetNodeId = targetId;
+        _x1 = x1; _y1 = y1; _x2 = x2; _y2 = y2;
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
--- GraphBuilder/Models/GraphNode.cs
+++ GraphBuilder/Models/GraphNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -7,18 +8,69 @@ namespace GraphBuilder.Models;
 public class GraphNode : INotifyPropertyChanged
 {
     public int Id { get; init; }
-    public double X { get; set; }
-    public double Y { get; set; }
-    public double Radius { get; set; } = 30.0;
-    public string Code { get; set; } = string.Empty;
-    
     public ObservableCollection<GraphEdge> OutgoingEdges { get; } = new();
+
+    private double _x;
+    public double X
+    {
+        get => _x;
+        set
+        {
+            if (Math.Abs(_x - value) > 0.001)
+            {
+                _x = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private double _y;
+    public double Y
+    {
+        get => _y;
+        set
+        {
+            if (Math.Abs(_y - value) > 0.001)
+            {
+                _y = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private double _radius = 30.0;
+    public double Radius
+    {
+        get => _radius;
+        set
+        {
+            if (Math.Abs(_radius - value) > 0.001)
+            {
+                _radius = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private string _code = string.Empty;
+    public string Code
+    {
+        get => _code;
+        set
+        {
+            if (_code != value)
+            {
+                _code = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     public GraphNode(int id, double x, double y)
     {
         Id = id;
-        X = x;
-        Y = y;
+        _x = x;
+        _y = y;
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
